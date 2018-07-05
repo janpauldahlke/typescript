382e0120ce64c9b0e6c0322048f0d22dec677dbb
janpauldahlke <janpauldahlke@gmail.com>
2018-07-05 09:51:56 +0200

    more on compiler options

--- compilercheck.js
+++ compilercheck.js
@@ -1,6 +1,17 @@
 "use strict";
 //having a look at compiled TS
 var myName = 'Paul';
-myName = 27; // it is compiled to working JS BUT the TSC errors
+//myName = 27 // it is compiled to working JS BUT the TSC errors
 console.log(typeof myName);
+//noImplicitAny: false
+var beAnything;
+beAnything = 24;
+//noImplicitAny: ture
+//TS 2.0
+// no unUsedParameter = true
+// strictNullCheck, else on result would be implicit null because no assign
+function controlMe(isTrue, somethingElse) {
+    var result;
+    return isTrue ? result = 12 : result; // not workking on TSC 2.0
+}
 //# sourceMappingURL=compilercheck.js.map
--- compilercheck.ts
+++ compilercheck.ts
@@ -1,5 +1,18 @@
 //having a look at compiled TS
 let myName : string = 'Paul';
-myName = 27 // it is compiled to working JS BUT the TSC errors
-
+//myName = 27 // it is compiled to working JS BUT the TSC errors
 console.log(typeof myName)
+
+//noImplicitAny: false
+let beAnything;
+beAnything = 24;
+//noImplicitAny: ture
+
+
+//TS 2.0
+// no unUsedParameter = true
+// strictNullCheck, else on result would be implicit null because no assign
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+  let result : number;
+  return isTrue ? result = 12 : result // not workking on TSC 2.0
+}
